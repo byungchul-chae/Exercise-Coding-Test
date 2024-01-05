52da348c6e91b92797a493b00ff84cb471956b20
byungchul-chae <151126586+byungchul-chae@users.noreply.github.com>
2024-01-05 09:56:37 +0900

    [level unrated] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,19 @@
+def solution(code):
+    ret = ''
+    mode = 0
+    for idx, i in enumerate(code):
+        if(mode):
+            if(i=='1'):
+                mode = 0
+            else:
+                if(idx%2):
+                    ret = ret + i
+        else:
+            if(i=='1'):
+                mode = 1
+            else:
+                if(idx%2==0):
+                    ret = ret + i
+    if(len(ret)==0):
+        return 'EMPTY'
+    return ret
