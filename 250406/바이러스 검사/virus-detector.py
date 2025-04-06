78477161965085fddb3788cf45916e91441f65a3
Byungchul Chae <151126586+byungchul-chae@users.noreply.github.com>
2025-04-06 13:19:52 +0900

    Title: 바이러스 검사, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250406/바이러스 검사/virus-detector.py
@@ -0,0 +1,14 @@
+n = int(input())
+people = []
+people.append(list(map(int,input().split())))
+ldr, mbr = map(int,input().split())
+
+result = n
+
+for i in range(n):
+    people[0][i] = people[0][i] - ldr
+    if(people[0][i]%mbr != 0):
+        result += 1
+    result += people[0][i] // mbr
+
+print(result)
