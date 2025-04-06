03259b72c25f05109fc331d84573270136f2a991
Byungchul Chae <151126586+byungchul-chae@users.noreply.github.com>
2025-04-06 13:27:10 +0900

    Title: 바이러스 검사, Time: 251ms, Memory: 108MB, Status: Passed - Codetree

--- 250406/바이러스 검사/virus-detector.py
+++ 250406/바이러스 검사/virus-detector.py
@@ -7,7 +7,7 @@ result = n
 
 for i in range(n):
     people[0][i] = people[0][i] - ldr
-    if people[0][i] > 0
+    if people[0][i] > 0:
         if(people[0][i]%mbr != 0):
             result += 1
         result += people[0][i] // mbr
