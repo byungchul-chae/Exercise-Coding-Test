682612ecb5eadd34c16a8d33f875901afcc03e69
Byungchul Chae <151126586+byungchul-chae@users.noreply.github.com>
2025-04-11 02:34:55 +0900

    Title: 고대 문명 유적 탐사, Time: 132ms, Memory: 20MB, Status: Passed - Codetree

--- 250411/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 250411/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -1,93 +1,90 @@
-K, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(5)]
-num = list(map(int, input().split()))
-ans = []
-
-
-# 우선순위
-# 회전수, 열, 행
-def rotate(arr, si, sj):
-    new_arr = [row[:] for row in arr]
+def rotate(arr, si, sj):    # 90도 시계방향 회전
+    narr = [x[:] for x in arr]
     for i in range(3):
         for j in range(3):
-            new_arr[si + i][sj + j] = arr[si + 3 - (j + 1)][sj + i]
-    return new_arr
+            narr[si+i][sj+j]=arr[si+3-j-1][sj+i]
+    return narr
 
+def bfs(arr,v,si,sj,clr):
+    q = []
+    sset = set()
+    cnt = 0
 
-from collections import deque
+    q.append((si,sj))
+    v[si][sj]=1
+    sset.add((si,sj))
+    cnt+=1
 
-
-def bfs(arr, v, si, sj):
-    umul = []
-    q = deque()
-    q.append((si, sj))
-    v[si][sj] = True
-    cnt = 1
     while q:
-        si, sj = q.popleft()
-        umul.append((si, sj))
+        ci,cj = q.pop(0)
+        # 네방향, 범위내, 미방문, 조건: 같은 값이면
+        for di,dj in ((-1,0),(1,0),(0,-1),(0,1)):
+            ni,nj = ci+di, cj+dj
+            if 0<=ni<5 and 0<=nj<5 and v[ni][nj]==0 and arr[ci][cj]==arr[ni][nj]:
+                q.append((ni,nj))
+                v[ni][nj]=1
+                sset.add((ni,nj))
+                cnt+=1
 
-        for di, dj in (-1, 0), (1, 0), (0, -1), (0, 1):
-            ni = si + di
-            nj = sj + dj
-            if (0 <= ni < 5 and 0 <= nj < 5 and arr[ni][nj] == arr[si][sj] and v[ni][nj] == False):
-                q.append((ni, nj))
-                v[ni][nj] = True
-                cnt += 1
-
-    if cnt >= 3:
-        for i, j in umul:
-            arr[i][j] = 0
+    if cnt>=3:      # 유물이면: cnt 리턴 + clr==1이면 0으로 clear
+        if clr==1:  # 0으로 초기화
+            for i,j in sset:
+                arr[i][j]=0
         return cnt
-    else:
+    else:           # 3개 미만이면 0리턴
         return 0
 
+def count_clear(arr, clr):  # clr==1인 경우 3개이상값들을 0으로 clear
+    v = [[0]*5 for _ in range(5)]
+    cnt = 0
+    for i in range(5):
+        for j in range(5):  # 미방문인 경우 같은 값이면 fill
+            if v[i][j]==0:
+                # 같은 값이면, 3개 이상인 경우
+                t = bfs(arr,v,i,j,clr)
+                cnt+=t
+    return cnt
 
-for _ in range(K):
-    max_num = 0
-    # [1] 탐사진행
-    for rot in range(1, 4):  # 0,1,2(90,180,270)
-        for r in range(3):
-            for c in range(3):
-                new_arr = [x[:] for x in arr]
-                for _ in range(rot):  # 회전하고
-                    new_arr = rotate(new_arr, r, c)
+K, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(5)]
+lst = list(map(int, input().split()))
+ans = []
 
-                # 유물 1차 획득
-                cnt = 0
-                v = [[False] * 5 for _ in range(5)]
-                for i in range(5):
-                    for j in range(5):
-                        if v[i][j] == False:
-                            n = bfs(new_arr, v, i, j)
-                            cnt += n
-                if cnt > max_num:
-                    max_num = cnt
-                    max_arr = [r[:] for r in new_arr]
-                # 최대 유물 개수 찾기 max_num update
-                # 배열 저장 max_arr = new_arr
-    if max_num == 0:
+for _ in range(K):  # K턴을 진행(유물이 없는 경우 즉시종료)
+    #[1] 탐사진행
+    mx_cnt = 0
+    for rot in range(1, 4):     # 회전수->열->행 (작은순)
+        for sj in range(3):
+            for si in range(3):
+                # rot 회수만큼 90도 시계방향 회전 => narr
+                narr = [x[:] for x in arr]
+                for _ in range(rot):
+                    narr = rotate(narr, si, sj)
+
+                # 유무개수 카운트
+                t = count_clear(narr, 0)
+                if mx_cnt < t:      # 최대개수
+                    mx_cnt = t
+                    marr = narr
+
+    # 유물이 없는 경우 턴 즉시종료
+    if mx_cnt==0:
         break
 
-    # [2] 연쇄획득
-    # max_배열에서 새로 채우고 다시 bfs
+    #[2] 연쇄획득
+    cnt = 0
+    arr = marr
     while True:
-        round_cnt = 0
+        t = count_clear(arr, 1)
+        if t==0:
+            break   # 연쇄획득 종료 => 다음 턴으로..
+        cnt += t    # 획득한 유물 개수 누적
+
+        # arr의 0값인 부분 리스트에서 순서대로 추가
         for j in range(5):
-            for i in range(4, -1, -1):
-                if max_arr[i][j] == 0:
-                    max_arr[i][j] = num.pop(0)
+            for i in range(4,-1,-1):
+                if arr[i][j]==0:
+                    arr[i][j]=lst.pop(0)
 
-        v = [[False] * 5 for _ in range(5)]
-        for i in range(5):
-            for j in range(5):
-                if not v[i][j]:
-                    round_cnt += bfs(max_arr, v, i, j)
-
-        max_num += round_cnt
-        if round_cnt == 0:
-            break
-    arr = [row[:] for row in max_arr]
-    ans.append(max_num)
-
+    ans.append(cnt) # 이번턴 연쇄획득한 개수 추가
 print(*ans)
